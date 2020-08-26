9d311b4aa630c6a6c43d4f29994d69fe5c8099eb
Vladik24051995 <x-outhink@mail.ru>
2020-08-26 10:25:55 +0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,32 @@
 import React from 'react'
-import { Route, Switch, Redirect, withRouter } from "react-router-dom"
+import { Route, Switch, Redirect, withRouter } from 'react-router-dom'
 import '@/assets/index.scss'
 import IntroLayout from '@/layouts/IntroLayout'
 import { getRouterByName } from '@/routes'
 
-let x;
-console.log(4454)
+console.log(4545)
 
 export const App: React.FC = () => {
   return (
     <div className="App">
       <Switch>
         <IntroLayout>
-          <Route path={getRouterByName('login-page').path} component={getRouterByName('login-page').component} />
-          <Route path={getRouterByName('registration-page').path} component={getRouterByName('registration-page').component} />
-          <Route path={getRouterByName('restore-password-page').path} component={getRouterByName('restore-password-page').component} />
-          <Redirect from={getRouterByName('login-page').path} to={getRouterByName('login-page').path}/>
+          <Route
+            path={getRouterByName('login-page').path}
+            component={getRouterByName('login-page').component}
+          />
+          <Route
+            path={getRouterByName('registration-page').path}
+            component={getRouterByName('registration-page').component}
+          />
+          <Route
+            path={getRouterByName('restore-password-page').path}
+            component={getRouterByName('restore-password-page').component}
+          />
+          <Redirect
+            from={getRouterByName('login-page').path}
+            to={getRouterByName('login-page').path}
+          />
         </IntroLayout>
       </Switch>
     </div>
